048dce44bc638df2c11e2695268eb86801e9b673
sujatamath <119061687+sujatamath@users.noreply.github.com>
2022-11-27 15:55:57 +0530

    Add files via upload

---
+++ social_link.py
@@ -0,0 +1,5 @@
+var = input("enter the website link: ")
+print("Social links- \nhttps://www.facebook.com/fulioTech/\nhttps://www.linkedin.com/company/ful-io/")
+print("Email/s-\nsupport@ful.io")
+print("Contact:\n+1 343 303 6668")
+
